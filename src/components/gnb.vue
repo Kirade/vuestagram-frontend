853836db29e839a8c6b7318c5f4f8b052d18be12
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-01-24 17:32:11 +0900

    ADD: svg logo

---
+++ src/components/gnb.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="gnb">
+    Global Navigation Bar
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+
+.gnb {
+  width: 100%;
+}
+</style>
