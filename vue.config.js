930c35bfd1dc599d7b76d903f56df3361ad296cb
JaeyoungHeo <jay.jaeyoung@gmail.com>
2020-01-25 01:43:00 +0900

    DEL: 불필요한 플러그인 옵션 제거

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,2 @@
 module.exports = {
-  pluginOptions: {
-    s3Deploy: {
-      registry: undefined,
-      awsProfile: 'default',
-      region: 'ap-northeast-2',
-      bucket: 'vuestagram-dev',
-      createBucket: false,
-      staticHosting: true,
-      staticIndexPage: 'index.html',
-      staticErrorPage: 'error.html',
-      assetPath: 'dist',
-      assetMatch: '**',
-      deployPath: '/',
-      acl: 'public-read',
-      pwa: false,
-      enableCloudfront: true,
-      uploadConcurrency: 5,
-      pluginVersion: '3.0.0',
-    },
-  },
 };
